6dbb291f641771512b68f7f8cf6c1def0dfebea7
ftreitasyuri <yuriftq@gmail.com>
2025-03-05 18:19:20 -0300

    Upload: Subindo o arquivo de exemplo 15 api, com todos as requisiões do crud

--- curso-vue/src/components/Exemplo15API.vue
+++ curso-vue/src/components/Exemplo15API.vue
@@ -7,8 +7,12 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 // Para trabalhar com a ingestão de dados, precisamos de um vetor
 import { ref, onMounted } from 'vue'
 
+
+// Vetor de produtos
 let produtos = ref([]);
 
+// Visibilidade dos botões
+let btnCadastrar = ref([]);
 
 // LifyCicle onMounted
 onMounted(() => {
@@ -21,36 +25,125 @@ onMounted(() => {
 // Objeto do tipo produto
 let obj = ref({ 'id': 0, 'produto': '', 'valor': 0 })
 
+
+
+
 // Função para cadastrar produtos
 
-function cadastrar(event){
+function cadastrar(event) {
     // alert('Teste')
 
 
     // Requisição de POST
-    fetch('http://localhost:3000/produtos',{
-        method:'POST',
-        body:JSON.stringify(obj.value),
-        headers:{'Content-Type': 'application/json'}
+    fetch('http://localhost:3000/produtos', {
+        method: 'POST',
+        body: JSON.stringify(obj.value),
+        headers: { 'Content-Type': 'application/json' }
     })
-    .then(requisicao => requisicao.json())
-    // Exemplo de debug abaixo
-    // .then(retorno => console.log(retorno))
-    .then(retorno => {
-        // Cadastrar o produto no vetor
-        produtos.value.push(retorno)
-        // Limpar os inputs
+        .then(requisicao => requisicao.json())
+        // Exemplo de debug abaixo
+        // .then(retorno => console.log(retorno))
+        .then(retorno => {
+            // Cadastrar o produto no vetor
+            produtos.value.push(retorno)
+            // Limpar os inputs
 
-        obj.value.produto = '',
-        obj.value.valor = 0
-    })
-
-       
-
-
+            obj.value.produto = '',
+                obj.value.valor = 0
+        })
 
     // Prevent Default
     event.preventDefault();
+}
+
+// Função para selecionar um produto especifico
+function selecionar(indice) {
+    // alert('Hello Selecionado')
+    obj.value = {
+        id: produtos.value[indice].id,
+        produto: produtos.value[indice].produto,
+        valor: produtos.value[indice].valor
+    }
+
+    btnCadastrar.value = false;
+}
+
+// Função para editar produtos
+function editar() {
+    // alert('Teste')
+
+
+    // Requisição de POST
+    fetch('http://localhost:3000/produtos/' + obj.value.id, {
+        // fetch(`http://localhost:3000/produtos/${obj.value.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(obj.value),
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(requisicao => requisicao.json())
+        // Exemplo de debug abaixo
+        // .then(retorno => console.log(retorno))
+        .then(retorno => {
+
+            // Obter o indice do vetor
+            let indiceProduto = produtos.value.findIndex(objP => {
+                return objP.id === retorno.id;
+            });
+
+            // alert(indiceProduto);
+
+            // Alterar a visibilidade dos botões 
+            btnCadastrar.value = true;
+
+            // Cadastrar o produto no vetor
+            produtos.value[indiceProduto] = retorno;
+            // Limpar os inputs
+            obj.value.id = 0;
+            obj.value.produto = '',
+                obj.value.valor = 0
+        })
+
+
+}
+// Função para remover produtos
+function remover() {
+    // alert('Teste')
+
+
+    // Requisição de POST
+    fetch('http://localhost:3000/produtos/' + obj.value.id, {
+        // fetch(`http://localhost:3000/produtos/${obj.value.id}`, {
+        method: 'DELETE',
+        // delete não tem BODY
+        // body: JSON.stringify(obj.value),
+        // headers: { 'Content-Type': 'application/json' }
+    })
+        .then(requisicao => requisicao.json())
+        // Exemplo de debug abaixo
+        // .then(retorno => console.log(retorno))
+        .then(retorno => {
+
+            // Obter o indice do vetor
+            let indiceProduto = produtos.value.findIndex(objP => {
+                return objP.id === obj.value.id;
+            });
+
+            // alert(indiceProduto);
+
+
+
+            // Remover o produto no vetor
+            produtos.value.splice(indiceProduto, 1);
+
+            // Alterar a visibilidade dos botões 
+            btnCadastrar.value = true;
+            // Limpar os inputs
+            obj.value.id = 0;
+            obj.value.produto = '',
+                obj.value.valor = 0
+        })
+
+
 }
 
 
@@ -62,13 +155,19 @@ function cadastrar(event){
     <!-- Formulário -->
 
     <form action="">
-        
+
         <!-- Para debug, comando abaixo para ver o v-model funcionanod -->
         <!-- <p>{{ obj }}</p> -->
 
+        <input type="hidden" v-model="obj.id">
         <input type="text" placeholder="Produto" class="form-control" v-model="obj.produto" id="produto" name="produto">
         <input type="text" placeholder="Valor" class="form-control" v-model="obj.valor" id="valor" name="valor">
-        <input type="submit" @click="cadastrar" value="Cadastrar" class="btn btn-primary" id="botao" name="botao">
+        <input type="submit" v-if="btnCadastrar" @click="cadastrar" value="Cadastrar" class="btn btn-primary" id="botao"
+            name="botao">
+
+        <input type="button" @click="editar" v-if="!btnCadastrar" value="Editar" class="btn btn-primary mx-2">
+
+        <input type="button" @click="remover()" v-if="!btnCadastrar" value="Excluir" class="btn btn-danger mx-2">
 
     </form>
 
@@ -82,10 +181,10 @@ function cadastrar(event){
             </tr>
         </thead>
         <tbody>
-            <tr v-for="v in produtos">
+            <tr v-for="(v, indice) in produtos">
                 <td>{{ v.produto }}</td>
                 <td>{{ v.valor }}</td>
-                <td><button class="btn btn-primary">Ver mais</button></td>
+                <td><button @click="selecionar(indice)" class="btn btn-primary">Ver mais</button></td>
             </tr>
         </tbody>
     </table>
